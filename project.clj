cb42ffc1ef1199368c028e9b6c38a5fb07518b7b
Phil Hofmann <phil@200ok.ch>
2023-06-02 15:34:58 +0200

    set group name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject genqref-lib "0.1.0"
+(defproject com.github.genqref/genqref-lib "0.1.0"
   :description "batteries-included API Wrapper for the great Space Traders API "
   :url "https://github.com/genqref/genqref-lib"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -10,5 +10,6 @@
                  [duratom "0.5.8"]
                  [cheshire "5.11.0"]]
   :repl-options {:init-ns genqref-lib.core}
+  :repositories [["releases" {:url "https://repo.clojars.org"}]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]])
