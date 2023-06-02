6a19876812dbed518d26bca600a750b36ef35a12
Phil Hofmann <phil@200ok.ch>
2023-06-02 15:05:15 +0200

    fix metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject genqref-lib "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "batteries-included API Wrapper for the great Space Traders API "
+  :url "https://github.com/genqref/genqref-lib"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
@@ -9,4 +9,6 @@
                  [com.taoensso/timbre "6.1.0"]
                  [duratom "0.5.8"]
                  [cheshire "5.11.0"]]
-  :repl-options {:init-ns genqref-lib.core})
+  :repl-options {:init-ns genqref-lib.core}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]])
